deabc5be87a221e8f7abe3d580e2a05212add075
89Explorer <leral3@naver.com>
2024-12-19 06:55:53 +0900

    Feat: 홈화면뷰컨트롤러 코드 작성

--- MeMe/HomeViewController.swift
+++ MeMe/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  MeMe
 //
 //  Created by 권정근 on 12/19/24.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
     }
 
+    
 
 }
 
