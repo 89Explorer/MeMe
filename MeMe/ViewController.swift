ddec854ebc35184dad1d19a4ec489e3494dddc6b
89Explorer <leral3@naver.com>
2024-12-19 06:21:17 +0900

    Initial Commit

---
+++ MeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeMe
+//
+//  Created by 권정근 on 12/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MeMeTests/MeMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeMeTests.swift
+//  MeMeTests
+//
+//  Created by 권정근 on 12/19/24.
+//
+
+import Testing
+@testable import MeMe
+
+struct MeMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
